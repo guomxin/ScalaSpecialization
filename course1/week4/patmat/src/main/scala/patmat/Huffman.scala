4467d9ee694579cceb916a56bb4cb518669661a7
guomxin <guomaoxin@foxmail.com>
2016-07-06 08:03:49 +0800

    partial work of convert function

--- course1/week4/patmat/src/main/scala/patmat/Huffman.scala
+++ course1/week4/patmat/src/main/scala/patmat/Huffman.scala
@@ -219,14 +219,23 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = tree match {
+    case Leaf(ch, _) => Nil
+    case Fork(left, right, _, _) => mergeCodeTables(convert(left), convert(right))
+  }
 
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+  def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = 
+    a match {
+    case Nil => Nil
+    case pair :: rpairs => (pair._1, 0 :: pair._2) :: 
+  }
+  
+  def 
 
   /**
    * This function encodes `text` according to the code tree `tree`.
