d4fe7ec14dfb808ef4f88d2a327931398a3d8e8a
guomxin <guomaoxin@foxmail.com>
2016-08-08 16:11:58 +0800

    0.2 version of course3 week2's assignment

--- course3/week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ course3/week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -46,7 +46,10 @@ object ParallelCountChange {
    *  coins for the specified amount of money.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    ???
+    if (money < 0) 0
+    else if (money == 0) 1
+    else if (coins.length == 0) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 
   type Threshold = (Int, List[Int]) => Boolean
@@ -55,20 +58,28 @@ object ParallelCountChange {
    *  specified list of coins for the specified amount of money.
    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    ???
+    if (money < 0) 0
+    else if (money == 0) 1
+    else if (coins.length == 0) 0
+    else if (threshold(money, coins)) countChange(money, coins)
+    else {
+      val (count1, count2) = parallel(
+          parCountChange(money - coins.head, coins, threshold), 
+          parCountChange(money, coins.tail, threshold))
+      count1 + count2
+    }
   }
 
   /** Threshold heuristic based on the starting money. */
   def moneyThreshold(startingMoney: Int): Threshold =
-    ???
+    (money: Int, _: List[Int]) => (money * 3 <= startingMoney * 2)
 
   /** Threshold heuristic based on the total number of initial coins. */
   def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ???
+    (_: Int, coins: List[Int]) => (coins.length * 3 <= totalCoins * 2)
 
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
-  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
-    ???
-  }
+  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = 
+    (money: Int, coins: List[Int]) => (2 * money * coins.length * 2 <= startingMoney * allCoins.length)
 }
